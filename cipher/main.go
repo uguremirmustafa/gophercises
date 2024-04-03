2fbf39b9a820ce6ec59755a8af6dd4acac91c826
uguremirmustafa <uguremirmustafa@gmail.com>
2024-04-03 19:37:16 +0300

    add cipher base

---
+++ cipher/main.go
@@ -0,0 +1,17 @@
+package main
+
+import (
+	"fmt"
+)
+
+func main() {
+	var length, delta int
+	var input string
+	fmt.Scanf("%d\n", &length)
+	fmt.Scanf("%s\n", &input)
+	fmt.Scanf("%d\n", &delta)
+
+	fmt.Println(length)
+	fmt.Println(input)
+	fmt.Println(delta)
+}
