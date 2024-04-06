6ac412a8be0676c3dd97f3af25ebbf078626b118
uguremirmustafa <uguremirmustafa@gmail.com>
2024-04-06 09:56:59 +0300

    init db package

--- task/main.go
+++ task/main.go
@@ -1,9 +1,25 @@
 package main
 
 import (
+	"fmt"
+	"os"
+	"path/filepath"
+
 	"github.com/uguremirmustafa/task/cmd"
+	"github.com/uguremirmustafa/task/db"
 )
 
 func main() {
-	cmd.RootCmd.Execute()
+
+	homePath, _ := os.UserHomeDir()
+	dbPath := filepath.Join(homePath, ".bolt.tasks.db")
+	must(db.Init(dbPath))
+	must(cmd.RootCmd.Execute())
+}
+
+func must(err error) {
+	if err != nil {
+		fmt.Println(err.Error())
+		os.Exit(1)
+	}
 }
